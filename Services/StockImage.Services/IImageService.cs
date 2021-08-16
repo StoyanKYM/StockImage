acc22363240401fc165484cc0bf30364b0330f6e
StoyanKYM <thunder_game@abv.bg>
2021-08-16 20:12:33 +0300

    added /create/all controller and ui for admin,user and unregistered users

---
+++ Services/StockImage.Services/IImageService.cs
@@ -0,0 +1,14 @@
+﻿using StockImage.Web.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockImage.Services
+{
+    public interface IImageService
+    {
+        Task<IQueryable<ImageViewModel>> GetAllImages();
+    }
+}
